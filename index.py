e11b9a0fa7035addcc411e6062227a6f73132af2
Brandon Mojica <Brann>
2023-01-11 16:42:11 -0600

    Ajustando programa

--- index.py
+++ index.py
@@ -1,33 +1,29 @@
 import pandas as pd
 import numpy as np
-import pyautogui, webbrowser
+import pyautogui
+import webbrowser
 from time import sleep
 import pyperclip as clipboard
 
 df = pd.read_excel("./telefonos.xlsx")
 numbers = np.array((df["Telefono"]))
 
-message = open("./message.txt", encoding="UTF-8")
-imagen = open("./img.jpg")
+message = open('./message.txt', encoding='utf-8')
 message = message.read()
 
 for num in numbers:
     webbrowser.open(f"https://web.whatsapp.com/send?phone={num}")
+    clipboard.copy(message)
 
     sleep(8)
-
     pyautogui.hotkey("ctrl", "v")
-    pyautogui.write(
-        """¡Hola :) !  ¡Disfruta de la nueva colección en *Elements Joyería!*
+    sleep(1)
+    pyautogui.hotkey("ctrl", "shift", "b")
+    sleep(1)
+    pyautogui.hotkey("ctrl", "v")
+    sleep(1)
+    pyautogui.press("enter")
+    sleep(1)
+    pyautogui.hotkey('ctrl', 'w')
 
-Realiza fácilmente tu pedido en : elements-of-steel.com.mx/catalog/ * 
-O con tu vendedor de confianza :)
-
-*Recuerda *ingresar tu número celular como usuario y contraseña*
-
-Si prefieres catálogo en PDF avísame ;D"""
-    )
-    # clipboard.copy(imagen)
-
-    # sleep(1)
-    # pyautogui.press("enter")
+    print(num)
