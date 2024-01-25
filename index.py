5e2385d5b2997a531b5cdce3b990109004fe1dfa
Brann <brandonmojica95@gmail.com>
2024-01-25 10:51:11 -0600

    actualizando sistema de envio de mensajes masivos

--- index.py
+++ index.py
@@ -1,29 +1,71 @@
+import re
 import pandas as pd
-import numpy as np
+from unidecode import unidecode
 import pyautogui
 import webbrowser
 from time import sleep
-import pyperclip as clipboard
 
-df = pd.read_excel("./telefonos.xlsx")
-numbers = np.array((df["Telefono"]))
 
-message = open('./message.txt', encoding='utf-8')
-message = message.read()
+try:
+    sleep_open = input(
+        "Por favor, ingresa el tiempo de espera para abrir la ventana (Ingresa un número entre 1 y 15):"
+    )
+    sleep_open = int(sleep_open)
 
-for num in numbers:
-    webbrowser.open(f"https://web.whatsapp.com/send?phone={num}")
-    clipboard.copy(message)
+    image = None
 
-    sleep(8)
-    pyautogui.hotkey("ctrl", "v")
-    sleep(1)
-    pyautogui.hotkey("ctrl", "shift", "b")
-    sleep(1)
-    pyautogui.hotkey("ctrl", "v")
-    sleep(1)
-    pyautogui.press("enter")
-    sleep(1)
-    pyautogui.hotkey('ctrl', 'w')
+    while not image == "SI" and not image == "NO":
+        image = input("El mensaje se enviara con imagen? (SI/NO)").strip().upper()
 
-    print(num)
+    if image == "SI":
+        image_load = input(
+            "Por favor, ingresa el tiempo de espera para cargar la imagen (Ingresa un número entre 1 y 15): "
+        )
+        image_load = int(image_load)
+
+    sleep_send = input(
+        "Por favor, ingresa el tiempo de espera despues de enviar el mensaje (Ingresa un número entre 1 y 15): "
+    )
+    sleep_send = int(sleep_send)
+
+    df = pd.read_excel("./telefonos.xlsx")
+
+    message = open("./message.txt", encoding="utf-8")
+    message = message.read()
+
+    for index, row in df.iterrows():
+        webbrowser.open(f"https://web.whatsapp.com/send?phone={row['Telefono']}")
+
+        sleep(sleep_open)
+        lines = message.split("\n")
+
+        for line in lines:
+            line = unidecode(line)
+            text = line
+            words_key = re.findall(r"\{(.*?)\}", line)
+
+            for word in words_key:
+                if word in df.columns:
+                    text = text.replace(f"{{{word}}}", str(df[word][index]))
+
+            pyautogui.write(text, interval=0.01)
+            pyautogui.hotkey("ctrl", "enter")
+
+        sleep(1)
+
+        if image == "SI":
+            pyautogui.hotkey("ctrl", "v")
+            sleep(image_load)
+
+        pyautogui.press("enter")
+        sleep(sleep_send)
+        pyautogui.hotkey("ctrl", "w")
+        sleep(5)
+        pyautogui.press("enter")
+
+        print(row["Telefono"])
+
+    print("El programa a finalizado con exito.")
+
+except ValueError:
+    print("Entrada no válida. Por favor, ingresa un número entero.")
